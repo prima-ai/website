83ef8e182b8c518ad6102527d5058b80f647499e
niiiils <fahrni.nils@gmail.com>
2019-04-27 17:58:46 +0200

    configured router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { TeamComponent } from './pages/team/team.component';
+import { AboutComponent } from './pages/about/about.component';
+
+const routes: Routes = [
+  { path: 'team', component: TeamComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TeamComponent } from './pages/team/team.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TeamComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
