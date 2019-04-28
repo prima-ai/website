d76c9e9e8629fd87ed4e2224db7de23ca6c31823
niiiils <fahrni.nils@gmail.com>
2019-04-28 18:23:52 +0200

    added landing page layout & navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { TeamComponent } from './pages/team/team.component';
 import { AboutComponent } from './pages/about/about.component';
+import { HubComponent } from './pages/hub/hub.component';
 
 const routes: Routes = [
   { path: 'team', component: TeamComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'hub', component: HubComponent },
   { path: '**', component: HomeComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { TeamComponent } from './pages/team/team.component';
 import { AboutComponent } from './pages/about/about.component';
+import { NavbarComponent } from './partials/navbar/navbar.component';
+import { FooterComponent } from './partials/footer/footer.component';
+import { HubComponent } from './pages/hub/hub.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     TeamComponent,
-    AboutComponent
+    AboutComponent,
+    NavbarComponent,
+    FooterComponent,
+    HubComponent
   ],
   imports: [
     BrowserModule,
