90c62068c87e51d38110377cd1e0b001766039c3
niiiils <fahrni.nils@gmail.com>
2019-04-29 23:49:47 +0200

    added functions to tag featured plugins

--- src/app/pages/hub/hub.component.ts
+++ src/app/pages/hub/hub.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CssSelector, collectExternalReferences } from '@angular/compiler';
+import { createChangeDetectorRef } from '@angular/core/src/view/refs';
 
 @Component({
   selector: 'app-hub',
@@ -6,17 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hub.component.scss']
 })
 export class HubComponent implements OnInit {
-  tags = 'yikes';
   constructor() { 
     
   }
 
   ngOnInit() {
-    this.fillTags();
+    this.fillTags(['#epic','#bruhmomentum']);
   }
 
-  fillTags() {
+  createTag(name :string) {
     let tagsection = document.getElementById('tags');
+    var cx = document.createElement('div');
+    cx.style.display = 'table-cell';
+    cx.textContent = name;
+    cx.style.backgroundColor = 'rgba(228, 233, 237, 0.9)'
+    cx.style.borderRadius = '10px';
+    cx.style.padding = '3px 6px';
+    tagsection.style.borderSpacing = '5px';
+    tagsection.appendChild(cx);
+    return tagsection;
+  }
+
+  fillTags(tags :string[]) {
+    for(var tag in tags) {
+      this.createTag(tags[tag]);
+    }
   }
 
 }
