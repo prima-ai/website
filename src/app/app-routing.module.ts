89d19458803b86c81cf692fb61277511f38d01ff
niiiils <fahrni.nils@gmail.com>
2019-06-24 10:32:08 +0200

    adjusted color scheme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { HomeComponent } from './pages/home/home.component';
 import { TeamComponent } from './pages/team/team.component';
 import { AboutComponent } from './pages/about/about.component';
-import { HubComponent } from './pages/hub/hub.component';
 import { GetstartedComponent } from './pages/getstarted/getstarted.component';
 import { BlogComponent } from './pages/blog/blog.component';
-import { ProfileComponent } from './pages/hub/users/profile/profile.component';
 
 const routes: Routes = [
   { path: 'team', component: TeamComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'hub', component: HubComponent },
   { path: 'getstarted', component: GetstartedComponent },
   { path: 'blog', component: BlogComponent },
-  { path: 'profile', component: ProfileComponent },
-  
   { path: '**', component: HomeComponent }
   
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,8 @@ import { TeamComponent } from './pages/team/team.component';
 import { AboutComponent } from './pages/about/about.component';
 import { NavbarComponent } from './partials/navbar/navbar.component';
 import { FooterComponent } from './partials/footer/footer.component';
-import { HubComponent } from './pages/hub/hub.component';
 import { GetstartedComponent } from './pages/getstarted/getstarted.component';
 import { BlogComponent } from './pages/blog/blog.component';
-import { ProfileComponent } from './pages/hub/users/profile/profile.component';
-import { FeaturedComponent } from './pages/hub/partials/featured/featured.component';
-import { ExploreComponent } from './pages/hub/partials/explore/explore.component';
-import { AppPreviewComponent } from './pages/hub/partials/app-preview/app-preview.component';
-import { SignupComponent } from './pages/hub/users/signup/signup.component';
-import { SigninComponent } from './pages/hub/users/signin/signin.component';
 
 @NgModule({
   declarations: [
@@ -26,15 +19,8 @@ import { SigninComponent } from './pages/hub/users/signin/signin.component';
     AboutComponent,
     NavbarComponent,
     FooterComponent,
-    HubComponent,
     GetstartedComponent,
     BlogComponent,
-    ProfileComponent,
-    FeaturedComponent,
-    ExploreComponent,
-    AppPreviewComponent,
-    SignupComponent,
-    SigninComponent
   ],
   imports: [
     BrowserModule,
